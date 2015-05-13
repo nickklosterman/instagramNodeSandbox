817f8263dc54042a107565b79898fee7c995403d
Klosterman <nklosterman@c02gycv5dv7l.limited.brands.com>
2015-05-13 15:10:09 -0400

    added sed resource to grabInstagramNames

--- grabInstagramNames.sh
+++ grabInstagramNames.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+
+#resources: http://www.opensourceforu.com/2011/04/sed-explained-part-1/
+
+#cut off from the _ followed by 6 digits back. that should handle names ending with numbers
 ls | sed 's/_[[:digit:]]\{6\}.*//' | uniq
